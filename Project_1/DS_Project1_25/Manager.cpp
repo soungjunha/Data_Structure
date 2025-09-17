9ee83fadc86644ea7a2c8b588b278f5bf0869752
soungjunha <soungjunha2018@gmail.com>
2025-09-18 01:32:12 +0900

    make run function

--- Project_1/DS_Project1_25/Manager.cpp
+++ Project_1/DS_Project1_25/Manager.cpp
@@ -15,7 +15,33 @@ Manager::~Manager() {
 }
 
 void Manager::run(const char* command) {
-	cout<<command;
+	ifstream command_file(command);
+
+	if (!command_file.is_open())
+	{
+		cout<<"not found file"<<endl;
+	}
+
+	string line;
+	while (getline(command_file, line))
+	{
+		string part_1,part_2;
+		size_t pos=line.find(' ');
+
+		if (pos != string::npos) {
+        	part_1 = line.substr(0, pos);
+        	part_2 = line.substr(pos + 1);
+    	} else {
+        	part_1 = line;
+        	part_2 = "";
+    	}
+
+		cout<<part_1<<endl;
+
+	}
+
+	command_file.close();
+
 }
 
 void Manager::LOAD() {
